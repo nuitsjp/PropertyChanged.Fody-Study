a1d220e74bfc7e420bd5c9a187293852c19286ad
nuitsjp <nuits.jp@live.jp>
2018-08-31 22:58:09 +0900

    Add Project.

---
+++ HelloFody/HelloFody/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HelloFody
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public string Message { get; set; } = "Hello, Fody!";
+
+        public ICommand UpdateMessage => new Command(() => Message = "Updated Message!!");
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
